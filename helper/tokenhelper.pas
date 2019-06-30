d9d7bd266887b9e8153be5f62cbeb7ba8fdf835c
Irfan Andriansyah <irfanandriansyah10@gmail.com>
2019-06-30 16:20:48 +0700

    feat(helper): create tokem helper

---
+++ helper/tokenhelper.pas
@@ -0,0 +1,45 @@
+unit TokenHelper;
+
+{$mode objfpc}{$H+}
+
+interface
+
+uses
+    Classes, SysUtils;
+
+type
+    TokenReaderHelper = class
+        function IsNumeric(Value: string; const AllowFloat: boolean): boolean;
+        function ExcludeSymbols(ch: char): boolean;
+    end;
+
+implementation
+
+function TokenReaderHelper.IsNumeric(Value: string; const AllowFloat: boolean): boolean;
+var
+    ValueInt: integer;
+    ValueFloat: extended;
+    ErrCode: integer;
+begin
+    Value := SysUtils.Trim(Value);
+    Val(Value, ValueInt, ErrCode);
+    result := ErrCode = 0;
+    if not result and AllowFloat then
+    begin
+
+        Val(Value, ValueFloat, ErrCode);
+        result := ErrCode = 0;
+    end;
+end;
+
+function TokenReaderHelper.ExcludeSymbols(ch: char): boolean;
+begin
+    ExcludeSymbols := (ch <> '+') and (ch <> '-') and (ch <> '*') and (ch <> '/') and
+        (ch <> '=') and (ch <> ':') and (ch <> ',') and (ch <> ';') and
+        (ch <> ' ') and (ch <> '(') and (ch <> ')') and (ch <> '[') and
+        (ch <> ']') and (ch <> '<') and (ch <> '>') and (ch <> #13) and
+        (ch <> #10) and (ch <> #9) and (ch <> '.');
+end;
+
+end.
+
